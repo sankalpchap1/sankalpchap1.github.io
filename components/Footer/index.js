364637b11168e21c3fdb639f98b1ac4aa9596a19
sankalpc7 <alexStraut@7798>
2023-03-02 00:20:54 -0600

    template-build

---
+++ components/Footer/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Socials from "../Socials";
+import Link from "next/link";
+import Button from "../Button";
+
+const Footer = ({}) => {
+  return (
+    <>
+      <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
+        <div>
+          <h1 className="text-2xl text-bold">Contact.</h1>
+          <div className="mt-10">
+            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
+              LET&apos;S WORK
+            </h1>
+            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
+              TOGETHER
+            </h1>
+            <Button type="primary">Schedule a call</Button>
+            <div className="mt-10">
+              <Socials />
+            </div>
+          </div>
+        </div>
+      </div>
+      <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
+        Made With ❤ by{" "}
+        <Link href="http://www.chetanverma.com">
+          <a className="underline underline-offset-1">Chetan Verma</a>
+        </Link>
+      </h1>
+    </>
+  );
+};
+
+export default Footer;

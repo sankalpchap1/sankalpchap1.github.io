93be14a0457f34292861808be040d95cc6b2e88a
sankalpc7 <alexStraut@7798>
2023-03-05 02:24:05 -0600

    adding favicon

--- components/WorkCard/index.js
+++ components/WorkCard/index.js
@@ -3,7 +3,13 @@ import { Card, CardMedia, CardContent, Typography, CardActions, Button } from '@
 
 const WorkCard = ({ img, name, description, onClick }) => {
   return (
-    <Card sx={{ maxWidth: 400, margin: '16px' }}>
+    <Card sx={{
+      maxWidth: 400, margin: '16px', transition: 'transform 0.3s ease, box-shadow 0.3s ease',
+      '&:hover': {
+        transform: 'scale(1.05)',
+        boxShadow: '0px 4px 20px rgba(0, 0, 0, 0.1)',
+      },
+    }}>
       <CardMedia
         sx={{ height: 250 }}
         image={img}

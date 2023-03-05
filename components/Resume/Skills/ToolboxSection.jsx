a505bc4e07eb6026bc91b634fc7e1b47d7935d11
sankalpc7 <alexStraut@7798>
2023-03-05 03:09:26 -0600

    techsectoion

--- components/Resume/Skills/ToolboxSection.jsx
+++ components/Resume/Skills/ToolboxSection.jsx
@@ -2,21 +2,27 @@ import { ToolboxIcon } from '../../Socials/icons'
 import React from 'react'
 import DevIcon from './DevIcon'
 import { devIcons } from './dev-icons'
+import Box from '@mui/material/Box';
+import Item from '@mui/material/ListItem'
 
 const TechSection = () => {
   return (
-    <section className="py-8">
+    <section className="py-24">
       <span className="flex items-center mb-8">
         <div className="bg-back-subtle p-2 mr-4 rounded-full">
           <ToolboxIcon />
         </div>
         <h4 className="text-xl text-accent font-semibold">Toolbox</h4>
       </span>
-      <ul className="grid grid-cols-2 sm:grid-cols-4 gap-6">
-        {devIcons.map(icon => (
-          <DevIcon name={icon.name} iconName={icon.iconName} key={icon.name} />
-        ))}
-      </ul>
+      <Box sx={{ flexGrow: 1 }}>
+        <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2}>
+          {devIcons.map(icon => (
+            <Box gridColumn="span 3">
+              <Item><DevIcon name={icon.name} iconName={icon.iconName} key={icon.name} /></Item>
+            </Box>
+          ))}
+          </Box>
+      </Box>
     </section>
   )
 }
--- pages/resume.js
+++ pages/resume.js
@@ -90,49 +90,6 @@ const Resume = () => {
                 )}
               </div>
               <ToolboxSection />
-              {/* <div className="mt-5">
-                <h1 className="text-2xl font-bold">Skills</h1>
-                <div className="flex mob:flex-col desktop:flex-row justify-between">
-                  {resume.languages && (
-                    <div className="mt-2 mob:mt-5">
-                      <h2 className="text-lg">Languages</h2>
-                      <ul className="list-disc">
-                        {resume.languages.map((language, index) => (
-                          <li key={index} className="ml-5 py-2">
-                            {language}
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  )}
-
-                  {resume.frameworks && (
-                    <div className="mt-2 mob:mt-5">
-                      <h2 className="text-lg">Frameworks</h2>
-                      <ul className="list-disc">
-                        {resume.frameworks.map((framework, index) => (
-                          <li key={index} className="ml-5 py-2">
-                            {framework}
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  )}
-
-                  {resume.others && (
-                    <div className="mt-2 mob:mt-5">
-                      <h2 className="text-lg">DBMS & Security</h2>
-                      <ul className="list-disc">
-                        {resume.others.map((other, index) => (
-                          <li key={index} className="ml-5 py-2">
-                            {other}
-                          </li>
-                        ))}
-                      </ul>
-                    </div>
-                  )}
-                </div>
-              </div> */}
             </div>
           </div>
         )}

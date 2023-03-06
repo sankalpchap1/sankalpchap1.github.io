2978447e37c38cbfa6a62fc2fad6e5af03dd5069
sankalpc7 <alexStraut@7798>
2023-03-05 23:53:20 -0600

    as

--- components/NewHeader/DesktopMenu.jsx
+++ components/NewHeader/DesktopMenu.jsx
@@ -20,7 +20,7 @@ export default function DesktopMenu(props) {
           delay: 0.4,
         }}
         className=" text-AAsecondary"
-       
+
       >
         <ReactScrollLink to="aboutSection" spy={true} smooth={true} offset={-100} duration={200}>
           &gt; 01. <span className="text-white hover:cursor-pointer hover:text-AAsecondary duration-300">About</span>
@@ -41,7 +41,7 @@ export default function DesktopMenu(props) {
           delay: 0.7,
         }}
         className="text-AAsecondary"
-       
+
       >
         <ReactScrollLink to="WhereIhaveWorkedSection" spy={true} smooth={true} offset={-300} duration={200}>
           &gt; 02.{" "}
@@ -65,10 +65,10 @@ export default function DesktopMenu(props) {
         className="text-AAsecondary"
       >
         <ReactScrollLink to="WorkCard" spy={true} smooth={true} offset={-100} duration={200}>
-        &gt; 03. <span className="text-white  hover:cursor-pointer hover:text-AAsecondary duration-300">Work</span>
+          &gt; 03. <span className="text-white  hover:cursor-pointer hover:text-AAsecondary duration-300">Work</span>
 
         </ReactScrollLink>
-        
+
       </motion.div>
       <motion.span
         initial={{
@@ -86,12 +86,12 @@ export default function DesktopMenu(props) {
         }}
         className="text-AAsecondary"
       >
-         <ReactScrollLink to="GetInTouchSection" spy={true} smooth={true} offset={-100} duration={200}>
-         &gt; 04. <span className="text-white  hover:cursor-pointer hover:text-AAsecondary duration-300">Contact</span>
+        <ReactScrollLink to="GetInTouchSection" spy={true} smooth={true} offset={-100} duration={200}>
+          &gt; 04. <span className="text-white  hover:cursor-pointer hover:text-AAsecondary duration-300">Contact</span>
         </ReactScrollLink>
       </motion.span>
-      <a href={"/sankalp_resume.pdf"} target={"_blank"} rel="noreferrer">
-      <motion.button
+
+      <motion.span
         initial={{
           y: -40,
           opacity: 0,
@@ -105,12 +105,35 @@ export default function DesktopMenu(props) {
           duration: 1.2,
           delay: 1.2,
         }}
-        className="text-AAsecondary border border-spacing-2 py-2 px-3 rounded-sm border-AAsecondary hover:bg-ResumeButtonHover"
+        className="text-AAsecondary"
       >
-        Resume
-      </motion.button>
+        <ReactScrollLink to="GetInTouchSection" spy={true} smooth={true} offset={-100} duration={200}>
+          &gt; 05. <span className="text-white  hover:cursor-pointer hover:text-AAsecondary duration-300">Blog</span>
+        </ReactScrollLink>
+      </motion.span>
+
+
+      <a href={"/sankalp_resume.pdf"} target={"_blank"} rel="noreferrer">
+        <motion.button
+          initial={{
+            y: -40,
+            opacity: 0,
+          }}
+          animate={{
+            y: 0,
+            opacity: 1,
+          }}
+          transition={{
+            type: "spring",
+            duration: 1.2,
+            delay: 1.2,
+          }}
+          className="text-AAsecondary border border-spacing-2 py-2 px-3 rounded-sm border-AAsecondary hover:bg-ResumeButtonHover"
+        >
+          Resume
+        </motion.button>
       </a>
-      
+
     </div>
   );
 }
--- components/NewHeader/Header.jsx
+++ components/NewHeader/Header.jsx
@@ -1,5 +1,4 @@
-import React, { useRef, useState, useEffect, useContext, HTMLDivElement } from "react";
-import PropTypes, { number } from 'prop-types';
+import React, { useRef, useState, useEffect, useContext } from "react";
 import Logo from "./Logo";
 import DesktopMenu from "./DesktopMenu";
 import IconMenu from "./IconMenu";
@@ -11,16 +10,16 @@ import AppContext from "./AppContext";
 //   ref.current?.classLIst.add(myclass);
 // };
 const Header = (props) => {
-  const RefNavBar = useRef<HTMLDivElement>(null);
+  const RefNavBar = useRef(null);
   const [ShowElement, setShowElement] = useState(false);
   // const [rotate, setRotate] = useState<boolean>(false);
   const context = useContext(AppContext);
-  const scrollSizeY=useRef(0);
+  const scrollSizeY = useRef(0);
 
   // Define the EventListener for the NavBar
   useEffect(() => {
     if (context.sharedState.portfolio.NavBar.IntervalEvent == null) {
-      context.sharedState.portfolio.NavBar.IntervalEvent=() => {
+      context.sharedState.portfolio.NavBar.IntervalEvent = () => {
         if (scrollSizeY.current == 0) {
           scrollSizeY.current = window.scrollY;
         } else {
@@ -54,7 +53,7 @@ const Header = (props) => {
     }
   }, [context.sharedState.portfolio.NavBar, context.sharedState.portfolio.NavBar.scrolling]);
 
-  
+
 
   useEffect(() => {
     setTimeout(() => {
@@ -62,7 +61,6 @@ const Header = (props) => {
     }, 400);
   }, []);
 
-  // console.log("rotate from header : ", rotate);
   //veify document for serverSide rendering
   if (typeof document !== "undefined") {
     document.body.style.overflow = "auto";
@@ -72,9 +70,8 @@ const Header = (props) => {
     <>
       {/* Mobile visible Navbar component, controlling ShowElement state to hide itself and rotate itself */}
       <MobileMenu setShowElement={setShowElement} ShowElement={ShowElement} />
-      {/* This parent element for Menu */}
       <motion.div
-        // ref={RefNavBar}
+        ref={RefNavBar}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         // changed from 10.4 to 1
@@ -90,10 +87,8 @@ const Header = (props) => {
           // setRotate={setRotate}
           setShowElement={setShowElement}
           ShowElement={ShowElement}
-          // finishedLoading={props.finishedLoading}
         />
 
-        {/* ? Desktop Menu by Titof */}
         <DesktopMenu />
       </motion.div>
     </>
